32ddd0362ff4412728fc7df72a5a7c9c140009e7
VikuDev69 <vikkubharti69@gmail.com>
2023-08-09 11:42:24 +0530

    Project Done

---
+++ lib/add_tod0.dart
@@ -0,0 +1,152 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AddTodoPage extends StatefulWidget {
+  final Map? todo;
+  const AddTodoPage({super.key, this.todo});
+
+  @override
+  State<AddTodoPage> createState() => _AddTodoPageState();
+}
+
+class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _descController = TextEditingController();
+  bool isEdit = false;
+  @override
+  void initState() {
+    super.initState();
+    final todo = widget.todo;
+    if (todo != null) {
+      isEdit = true;
+      final title = todo['title'];
+      final description = todo['description'];
+      _titleController.text = title;
+      _descController.text = description;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.deepPurple.shade200,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.deepPurple.shade500,
+          title: Text(
+            isEdit ? "Edit Note" : "Add Todo",
+            style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 24,
+                color: Colors.white60),
+          ),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.all(20),
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(hintText: "Tittle"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: _descController,
+              decoration: InputDecoration(hintText: "Description"),
+              keyboardType: TextInputType.multiline,
+              minLines: 5,
+              maxLines: 8,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  isEdit ? UpdateData() : submitData();
+                },
+                child: Text(isEdit ? "Update" : "Add"))
+          ],
+        ));
+  }
+
+  Future<void> UpdateData() async {
+    final todo = widget.todo;
+    if (todo == null) {
+      print("You can not update todo with empty data");
+      return;
+    }
+
+    final id = todo['_id'];
+    final isCompleted = todo['is_completed'];
+
+    // get data from form
+    final title = _titleController.text;
+    final description = _descController.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": isCompleted
+    };
+
+    // and Then Will submit to server
+    final url = 'https://api.nstack.in/v1/todos/$id';
+    final uri = Uri.parse(url);
+    final response = await http.put(uri,
+        body: jsonEncode(body), headers: {'Content-Type': "application/json"});
+    if (response.statusCode == 200) {
+      showSuccessMessage('Updated Successfully');
+    } else {
+      showErrorMessage('Updation Error');
+    }
+  }
+
+  Future<void> submitData() async {
+    // get data from form
+
+    final title = _titleController.text;
+    final description = _descController.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false
+    };
+
+    // and Then Will submit to server
+    final url = 'https://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+    final response = await http.post(uri,
+        body: jsonEncode(body), headers: {'Content-Type': "application/json"});
+    if (response.statusCode == 201) {
+      _titleController.text = "";
+      _descController.text = "";
+      showSuccessMessage('Created Successfully');
+    } else {
+      showErrorMessage('Creation Error');
+    }
+  }
+
+  // also a Success of Failing msg will be Shown
+  void showSuccessMessage(String message) {
+    final snackBar =
+        SnackBar(duration: Duration(seconds: 1), content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+      duration: Duration(seconds: 1),
+      content: Text(
+        message,
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,186 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:todo_with_api/add_tod0.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isLoading = true;
+  List items = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.deepPurple.shade200,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.deepPurple.shade500,
+          title: const Text(
+            "Todo",
+            style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 24,
+                color: Colors.white60),
+          ),
+          centerTitle: true,
+        ),
+        body: Visibility(
+          visible: isLoading,
+          replacement: RefreshIndicator(
+            onRefresh: fetchTodo,
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index] as Map;
+                final id = item['_id'] as String;
+                return Card(
+                  margin: const EdgeInsets.all(15),
+                  child: ListTile(
+                    tileColor: Colors.deepPurple.shade100,
+                    splashColor: Colors.deepPurple,
+                    title: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      child: Text(
+                        item['title'],
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    subtitle: Text(
+                      item['description'],
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                            onPressed: () {
+                              navigateToEditPage(item);
+                            },
+                            icon: const Icon(
+                              Icons.edit,
+                              color: Colors.deepPurple,
+                            )),
+                        IconButton(
+                            onPressed: () {
+                              deletedById(id);
+                            },
+                            icon: const Icon(
+                              Icons.delete,
+                              color: Colors.red,
+                            )),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          child: const Center(
+              child: CircularProgressIndicator(
+            color: Colors.white,
+          )),
+        ),
+        floatingActionButton: FloatingActionButton(
+          tooltip: "Add",
+          splashColor: Colors.deepPurple,
+          onPressed: () => navigateToAddPage(),
+          child: const Icon(
+            Icons.add,
+            size: 30,
+          ),
+        ));
+  }
+
+  // Go To Add Page
+  Future<void> navigateToAddPage() async {
+    final rout = MaterialPageRoute(
+      builder: (context) => AddTodoPage(),
+    );
+    await Navigator.push(context, rout);
+    setState(() {
+      isLoading = true;
+    });
+    fetchTodo();
+  }
+
+  //Go To Edit page
+  Future<void> navigateToEditPage(Map item) async {
+    final rout = MaterialPageRoute(
+      builder: (context) => AddTodoPage(todo: item),
+    );
+    Navigator.push(context, rout);
+    setState(() {
+      isLoading = true;
+    });
+    fetchTodo();
+  }
+
+  // Delete Todo
+  Future<void> deletedById(String id) async {
+    //1 we will Delete The Item from server
+    final url = "https://api.nstack.in/v1/todos/$id";
+    final uri = await Uri.parse(url);
+    final response = await http.delete(uri);
+    // Delete The Item From list
+    if (response.statusCode == 200) {
+      final filteredItem =
+          items.where((element) => element['_id'] != id).toList();
+      setState(() {
+        items = filteredItem;
+        showSuccessMessage('Todo Deleted');
+      });
+    } else {
+      //Show Error
+      showErrorMessage('Failed To Delete');
+    }
+  }
+
+  // Get All Todo
+  Future<void> fetchTodo() async {
+    final url = "https://api.nstack.in/v1/todos?page=1&limit=10";
+    final uri = await Uri.parse(url);
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final result = json['items'] as List;
+      print(response.body);
+      setState(() {
+        items = result;
+      });
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  void showSuccessMessage(String message) {
+    final snackBar =
+        SnackBar(duration: Duration(seconds: 1), content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+      duration: Duration(seconds: 1),
+      content: Text(
+        message,
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
